12a45b3566a957c25b3a78a25f615064371b97af
Tyrone <tyrone03473@hotmail.com>
2020-02-16 02:14:46 +0800

    update

--- ISAD251Assignment/ISAD251Assignment/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISAD251Assignment.MvcApplication" Language="C#" %>
---
+++ ISAD251Assignment/ISAD251Assignment/ISAD251Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISAD251Assignment.Startup))]
+namespace ISAD251Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
