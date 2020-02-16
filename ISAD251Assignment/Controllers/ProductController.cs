e4ceddf885e00cf18b4dc7e0eefe8dc4d39f5445
Tyrone <tyrone03473@hotmail.com>
2020-02-16 17:56:50 +0800

    SQL & Create Product

---
+++ ISAD251Assignment/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISAD251Assignment.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Models.Product> result = new List<Models.Product>();
+
+            using (Models.CartsEntities db = new Models.CartsEntities())
+
+                result = (from s in db.Products select s).ToList();
+
+                return View(result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Create(Models.Product postback)
+        {
+            using (Models.CartsEntities db = new Models.CartsEntities())
+            {
+                db.Products.Add(postback);
+
+                db.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ ISAD251Assignment/Models/Carts.edmx.sql
@@ -0,0 +1,58 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 02/16/2020 17:17:06
+-- Generated from EDMX file: C:\Users\ncgb03473\Desktop\Database Assignment\ISAD251Assignment\ISAD251Assignment\Models\Carts.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [Carts];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Products]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Products];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Products'
+CREATE TABLE [dbo].[Products] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(100)  NOT NULL,
+    [Price] int  NOT NULL,
+    [Stock] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Products'
+ALTER TABLE [dbo].[Products]
+ADD CONSTRAINT [PK_Products]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
